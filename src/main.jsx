8b93d4dd024060ddd76f8547b6454c4fd15424b9
mohamed khattab <mkhattab369@gmail.com>
2023-10-29 11:19:26 +0200

    creating reusable star rating component with understanding of props

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from './App.jsx'
+// import './index.css'
+import StarRating from "./components/StarRating";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <StarRating maxNumberOfStars={5} />
+  </React.StrictMode>
+);
