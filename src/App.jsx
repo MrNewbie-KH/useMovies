0b6e18ad57451980a8accdf21ae22e7fb2325c24
mohamed khattab <mkhattab369@gmail.com>
2023-10-30 19:18:50 +0200

    useEffect to handle rendering on change

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,42 @@ import MovieList from "./components/Movielist";
 import WatchedList from "./components/WatchedList";
 import Watchedsummary from "./components/Watchedsummary";
 import Loader from "./components/Loader";
+import Error from "./components/Error";
 
 export default function App() {
   // top level code is render logic
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
-  const [query, setQuery] = useState("batman");
+  const [query, setQuery] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState("");
   // here re-rendering is done million times
   useEffect(() => {
-    setIsLoading(true);
     async function fetchData() {
-      const res = await fetch(
-        `http://www.omdbapi.com/?i=tt3896198&apikey=272af9b1&s=${query}`
-      );
-      const data = await res.json();
-      setMovies(data.Search);
-      setIsLoading(false);
+      try {
+        setIsLoading(true);
+        setIsError("");
+        const res = await fetch(
+          `http://www.omdbapi.com/?i=tt3896198&apikey=272af9b1&s=${query}`
+        );
+        // user lose connection
+        if (!res.ok) throw new Error("Failed to fetch");
+        const data = await res.json();
+        if (data.Response === "False") throw new Error("No movies");
+        setMovies(data.Search);
+      } catch (error) {
+        setIsError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+      if (!query.length) {
+        setMovies([]);
+        setIsError("");
+        return;
+      }
     }
     fetchData();
-  }, []);
+  }, [query]);
 
   return (
     <>
@@ -40,7 +56,15 @@ export default function App() {
         </p>
       </Navbar>
       <Main>
-        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
+        <Box>
+          {isError ? (
+            <Error message={isError} />
+          ) : isLoading ? (
+            <Loader />
+          ) : (
+            <MovieList movies={movies} />
+          )}
+        </Box>
         <Box>
           <>
             <Watchedsummary watched={watched} movies={movies} />
---
+++ src/components/Error.jsx
@@ -0,0 +1,4 @@
+function Error({ message }) {
+  return <p className="error">{message}</p>;
+}
+export default Error;
